e91e8315c3695039693e669abf111c66168d7de4
오늘한 거 <sksslssn@gmail.com>
2025-05-26 18:46:56 +0900

    Revert "오늘한 거"
    This reverts commit d6c9c584be8640be032236ca2a16ac6ec675fdc4.

--- JAVA04_control/src/kh/java/start/Start.java
+++ JAVA04_control/src/kh/java/start/Start.java
@@ -1,14 +1,17 @@
 package kh.java.start;
 
-import kh.java.func.LoopTest;
+import kh.java.func.SwitchTest;
+import kh.java.func.forTest;
 
 public class Start {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		SwitchTest st = new SwitchTest();
 		//st.test5();
 
-		LoopTest lt = new LoopTest();
-		lt.star7();
+		forTest ft = new forTest();
+		ft.test2();
+
 	}
 }
